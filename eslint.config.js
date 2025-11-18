4edc80afb484dc8345477da2324c23de790d53c3
gudaoxuri <i@sunisle.org>
2025-11-18 13:28:54 +0800

    refactor: update ESLint configuration to include additional ignored directories

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,12 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   svelte: true,
+  ignores: [
+    'build/',
+    '.svelte-kit/',
+    'dist/',
+    'node_modules/',
+    'src-tauri/',
+    '.github/'
+  ]
 })
